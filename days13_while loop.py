a8567502335998dd7592f82a24f17a65c5b4943c
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-10-23 07:21:09 -0700

    Add files via upload

---
+++ days13_while loop.py
@@ -0,0 +1,17 @@
+#while Loop
+#contoh 1
+#angka = 10
+while angka > 8: #ketika nilainya true sistem akan melakukan aksi tanpa terbatas
+    print("while loop")
+
+#contoh 2
+
+umur = 0
+while umur < 18:
+    umur = umur + 1
+    print("ulang tahun ke",umur)
+
+
+#perbedaan for dan while loop
+# for sudah ditentukan sebelumnya berapa loop (perulangan yang dilakukan)
+# sedangkan while perulangannya tidak menentu berapa jumlah perulangan
